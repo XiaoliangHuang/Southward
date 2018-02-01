23de06558e1a00033afd39a329e6e21d412122dc
laomanco <laomanco@gmail.com>
2018-02-02 00:42:25 +0800

    update

---
+++ LittleProgram/pages/about/about.js
@@ -0,0 +1,13 @@
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    movies: [
+      { url: '/img/members/sd.png' },
+      { url: '/img/members/zhongyan.png' },
+      { url: '/img/members/handsome.png' }
+    ]
+  },
+  onLoad: function () {
+  }
+})    
